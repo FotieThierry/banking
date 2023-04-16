210325d68cdbf0bbe1bbb0f1d2c7d079492b8147
FotieThierry <fotiethierry7@gmail.com>
2023-04-16 13:15:36 +0200

    initialisation du projet

---
+++ src/test/java/com/fotie/banking/BankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fotie.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
